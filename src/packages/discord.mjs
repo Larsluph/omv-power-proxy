7de2dedbc0680b940c32f0e085c19e17dd326875
Rémi Marseault <larsluph@larsluph.dev>
2025-02-14 22:49:29 +0100

    perf(discord): Log to console when secrets not set

--- src/packages/discord.mjs
+++ src/packages/discord.mjs
@@ -1,11 +1,25 @@
+/** @typedef { { embeds: [{ title: string, color: number, timestamp: string }]}} Payload */
+
+/**
+ * Generates a Discord payload valid for a webhook
+ * @param title {string} - Webhook message title
+ * @param color {number} - Webhook message color (hex)
+ * @returns Payload - The generated payload
+ */
 export function genPayload(title, color) {
   return { embeds: [{ title, color, timestamp: new Date().toISOString() }] }
 }
 
+/**
+ * Sends a payload to the configured Discord webhook
+ * @param payload {Payload} - The payload to send
+ * @returns {Promise<void>}
+ */
 export async function sendWebhook(payload) {
   const { DISCORD_WEBHOOK_ID, DISCORD_WEBHOOK_TOKEN } = process.env
   if (!DISCORD_WEBHOOK_ID || !DISCORD_WEBHOOK_TOKEN) {
-    console.warn('Webhook environment variables not set, skipping')
+    const { title, timestamp } = payload.embeds[0]
+    console.log(`[${timestamp}] ${title}`)
     return
   }
 
